3db7cfbdb8720d221ee7a5ce0b1ac4d33386bc02
Levi Waldron <lwaldron.research@gmail.com>
2018-07-03 16:22:26 +0200

    Travis integration with automatic map updates

---
+++ .pre_script_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GH_REPO="@github.com/waldronlab/HGNChelper.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+
+pwd
+cd inst/
+R CMD BATCH --vanilla hgncLookup.R
+cd ..
+
+git init
+git config user.name "lwaldron-travis"
+git config user.email "travis"
+
+git stage data/hgnc.table.rda
+git commit -m "updated HGNC map"
+git push --force --quiet $FULL_REPO master
