7f6079dd1fcb07d26b3f775af22e4a59abfee18f
Levi Waldron <lwaldron.research@gmail.com>
2018-07-03 22:05:39 +0200

    Try Travis hook again

--- .pre_script_build.sh
+++ .pre_script_build.sh
@@ -5,13 +5,16 @@ FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
 pwd
 cd inst/
-R CMD BATCH --vanilla hgncLookup.R
+Rscript --vanilla hgncLookup.R
 cd ..
 
 git init
 git config user.name "lwaldron-travis"
 git config user.email "travis"
 
+git branch newmap
+git checkout newmap
 git stage data/hgnc.table.rda
 git commit -m "updated HGNC map"
-git push --force --quiet $FULL_REPO master
+git push --force --quiet $FULL_REPO master:newmap
+
