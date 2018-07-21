d97e04587ac49097801f69e722aef2f6a8578cc5
Levi Waldron <lwaldron.research@gmail.com>
2018-07-21 17:23:42 -0400

    commented-out instructions for updating gh-pages

--- update.sh
+++ update.sh
@@ -4,3 +4,7 @@ save(hgnc.table, file='data/hgnc.table.rda', compress='bzip2'); \
 mouse.table <- HGNChelper::getCurrentMouseMap(); \
 save(mouse.table, file='data/mouse.table.rda', compress='bzip2'); \
 roxygen2::roxygenize()"
+
+# for publishing the vignette to gh-pages
+# Rscript --vanilla -e "devtools::build_vignettes()"
+# ghp-import inst/doc
